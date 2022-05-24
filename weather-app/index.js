593a3c297ad0201580efe7d595e1681ee15e9577
Noam Neeman <noamne@monday.com>
2022-05-24 09:42:52 +0300

    feat: add weather app

--- weather-app/index.js
+++ weather-app/index.js
@@ -1,17 +1,28 @@
 import { AsyncWeather } from "@cicciosgamino/openweather-apis";
 import http from "http";
-// import your own logger
+import { mondayuLogger } from "mondayu-logger";
 
 // step 1: install lib and print temp to console
-const apiKey = "<<your api key>>";
-const weatherInitializer = new AsyncWeather();
-const weatherAPI = await weatherInitializer;
+const apiKey = ""; // never store a key in your source code and if you do, revoke its access or deactivate it.
+const weatherInstance = new AsyncWeather();
+const weatherAPI = await weatherInstance;
 
 // step 2: server
 // create your server here from http module
 // example in the slides or here: https://nodejs.org/en/knowledge/HTTP/servers/how-to-create-a-HTTP-server/
+const server = http.createServer(async (req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  weatherAPI.setApiKey(apiKey);
+  weatherAPI.setCityId(293397);
+  const temp = await weatherAPI.getTemperature();
+  res.end(
+    JSON.stringify({
+      temp,
+    })
+  );
+  mondayuLogger.log(`temp is ${temp}`);
+});
 
 // start your server
-
-// step 3: install your logger and log temp and server start
-// log that the service has started
+server.listen(8000);
+mondayuLogger.log('weather service started! 🌤🌧🌈');
