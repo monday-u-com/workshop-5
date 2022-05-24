757721422d150169999b5f07a8d11e3f7e922aad
Noam Neeman <noamne@monday.com>
2022-05-24 09:53:56 +0300

    Revert "feat: add logger"
    This reverts commit b0990092c6a90422d9dd0bc6b06c45d10203a5c1.

--- mondayu-logger/index.js
+++ mondayu-logger/index.js
@@ -2,11 +2,6 @@ import { Console } from 'console';
 import { createWriteStream } from 'fs';
 
 // step 1: use this example: https://melvingeorge.me/blog/save-logs-to-files-nodejs
-export const mondayuLogger = new Console({
-    stdout: createWriteStream('log.txt', { flags: 'a' }),
-});
+// export your monday logger here
 
-// step 2: check your logger works
-mondayuLogger.log('hello mondayU! it works!');
-
-// step 3: rename the package and publish 
+// step 2: rename the package and publish 
