b0990092c6a90422d9dd0bc6b06c45d10203a5c1
Noam Neeman <noamne@monday.com>
2022-05-24 09:42:38 +0300

    feat: add logger

--- mondayu-logger/index.js
+++ mondayu-logger/index.js
@@ -2,6 +2,11 @@ import { Console } from 'console';
 import { createWriteStream } from 'fs';
 
 // step 1: use this example: https://melvingeorge.me/blog/save-logs-to-files-nodejs
-// export your monday logger here
+export const mondayuLogger = new Console({
+    stdout: createWriteStream('log.txt', { flags: 'a' }),
+});
 
-// step 2: rename the package and publish 
+// step 2: check your logger works
+mondayuLogger.log('hello mondayU! it works!');
+
+// step 3: rename the package and publish 
